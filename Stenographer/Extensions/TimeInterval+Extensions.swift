60765c488a3441f6ab2eeca3f8bd0402184fabe7
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2026-01-13 12:10:26 -0300

    Add SwiftLint and SwiftFormat

--- Stenographer/Extensions/TimeInterval+Extensions.swift
+++ Stenographer/Extensions/TimeInterval+Extensions.swift
@@ -4,12 +4,12 @@ extension TimeInterval {
 
     var srtTimecode: String {
         let totalMilliseconds = Int(self * 1000)
-        let ms = totalMilliseconds % 1000
+        let milliseconds = totalMilliseconds % 1000
         let totalSeconds = totalMilliseconds / 1000
-        let s = totalSeconds % 60
-        let m = (totalSeconds / 60) % 60
-        let h = totalSeconds / 3600
+        let seconds = totalSeconds % 60
+        let minutes = (totalSeconds / 60) % 60
+        let hours = totalSeconds / 3600
 
-        return String(format: "%02d:%02d:%02d,%03d", h, m, s, ms)
+        return String(format: "%02d:%02d:%02d,%03d", hours, minutes, seconds, milliseconds)
     }
 }
